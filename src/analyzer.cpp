06df985e1e92664245075ce67939dfaeb13b70cd
raequin <raequin@gmail.com>
2019-08-23 10:41:26 -0400

    Adding by_week tracking

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include<ctime>  // For day of week from date etc.
+#include <ctime>  // For day of week from date etc.
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -137,7 +137,41 @@ std::istream& operator>>(std::istream& str, CheckinsFileRow& data)
 {
   data.readNextRow(str);
   return str;
-} 
+}
+
+
+
+std::vector<std::string> GetTutorsList(int semester_index) {
+  std::vector<std::vector<std::string>> tutor_names_lists;
+  std::vector<std::string> tutor_names  = {"Marshall, Matthew", "Wangenheim, Evan", "Hyatt, Haylee",
+					   "Freeman, Malik", "Wiggins, Robert", "Williams, Myles", "Williams, Bradley",
+					   "Daoudi, Karim", "Rogers, Cassidy", "Lin, Min Chih", "Colley, Jonny"};
+  tutor_names_lists.push_back(tutor_names);  // Spring 2019
+  tutor_names  = {"Marshall, Matthew", "Freeman, Malik", "Wiggins, Robert", "Williams, Bradley", "Rogers, Cassidy",
+		  "Lin, Min Chih", "Amoakon, Lythia", "Patel, Jay", "Hiller, Aaron", "Giles, John", "Dedani, Ashween", "Mistry, Nensi"};
+  tutor_names_lists.push_back(tutor_names);  // Summer 2019
+
+  return tutor_names_lists[semester_index];
+}
+
+
+
+std::vector<std::string> GetCoursesList(int semester_index) {
+  std::vector<std::vector<std::string>> course_names_lists;
+  std::vector<std::string> course_names  = {"CE-2003", "EE-2301", "ECET-1101", "ECET-1101L", "EE-2305", "EE-3401", "ECET-2300", "ECET-2300L", "ECET-2310", "ECET-2310L",
+					    "ENGR-2214", "MET-2124", "ENGR-3122", "MET-3126", "ENGR-3131", "ENGR-3132", "MET-3124", "MET-3124L",
+					    "ENGR-3343", "ENGR-3345", "MET-3101", "ISYE-2600", "IET-2227", "ISYE-3400", "IET-4405", "ME-4141", "MET-4141", "MTRE-2610"};
+  course_names_lists.push_back(course_names);  // Spring 2019
+  course_names_lists.push_back(course_names);  // Summer 2019
+  course_names  = {"ENGR-2214", "MET-2124", "ENGR-3131", "ENGR-3132", "MET-3124", "MET-3124L", "EDG-1210", "EDG-1211", "EDG-3112", "EDG-2160",
+		   "EE-2301", "ECET-1101", "ECET-1101L", "EE-2305", "EE-2501", "ECET-1200", "ECET-1200L", "ECET-2210", "ECET-2210L", "EE-2401",
+		   "EE-3401", "ECET-2300", "ECET-2300L", "ECET-2310", "ECET-2310L",
+		   "ENGR-3122", "MET-3126", "MTRE-2610", "ME-3101", "MET-3132", "MET-3132L", "ME-3701", "MET-1311", "ISYE-3350", "IET-2249"};
+  course_names_lists.push_back(course_names);  // Fall 2019
+
+  return course_names_lists[semester_index];
+
+}
 
 
 
@@ -145,13 +179,10 @@ int main(int argc,  char **argv)
 {
   std::vector<Tutee> tutees;
   const uint8_t initial_line_num = 9;  // Where the data start on the txt file, starting at 0
-  std::vector<std::string> tutor_names  = {"Marshall, Matthew", "Wangenheim, Evan", "Hyatt, Haylee",
-					   "Freeman, Malik", "Wiggins, Robert", "Williams, Myles", "Williams, Bradley",
-					   "Daoudi, Karim", "Rogers, Cassidy", "Lin, Min Chih", "Colley, Jonny",
-					   "Amoakon, Lythia", "Patel, Jay", "Hiller, Aaron", "Giles, John",
-					   "Dedani, Ashween", "Freitas da Mata Filho, Luiz", "Mistry, Nensi"};
   string complete_path(argv[1]);
   string analysis_mode(argv[2]);
+  std::vector<std::string> tutor_names = GetTutorsList(std::stoi(argv[3]));  // Which list of tutors to filter out of checkins
+  std::vector<std::string> course_names = GetCoursesList(std::stoi(argv[3]));  // Which list of courses to use for filtering checkins
   std::ifstream file(complete_path);
   CheckinsFileRow row;
 
@@ -163,21 +194,39 @@ int main(int argc,  char **argv)
 
   // Place data into vector of cells, one row at a time
   while (file >> row) {
+    // Debug
+    /*
+    cout << "size of row = " << row.size() << std::endl;
+    for (std::size_t j = 0; j < row.size(); j++)
+      cout << row[j] << "\n";
+    cout << std::endl;
+    */
+    
     string this_name (row[3].begin()+1, row[3].end()-1);  // Remove quotation marks around name
+    string this_course (row[7]);
     
     // Ensure row is not for a tutor swipe created when testing the kiosk
     bool is_tutor = false;
     for (auto n : tutor_names) {
-      if (0 == n.compare(this_name))  // "name" cell matches entry on the tutor list
+      if (0 == n.compare(this_name)) {  // "name" cell matches entry on the tutor list
 	is_tutor = true;
+	break;
+      }
     }
 
-    if (!is_tutor) {
+    // Ensure row is for a valid course
+    bool is_valid_course = false;
+    for (std::vector<std::string>::size_type i = 0; i < course_names.size(); ++i) {
+      if (0 == course_names[i].compare(this_course)) {  // "name" cell matches entry on the course list
+	is_valid_course = true;
+	break;
+      }
+    }
+
+    if (!is_tutor && is_valid_course) {
       // Create new Tutee if needed, otherwise add Visit to existing Tutee
       bool is_new_tutee = true;
-      for(std::vector<Tutee>::size_type i = 0; i < tutees.size(); ++i) {
-	//      for(std::vector<Tutee>::iterator it = tutees.begin(); it != tutees.end(); ++it) {
-	
+      for (std::vector<Tutee>::size_type i = 0; i < tutees.size(); ++i) {	
 	if (0 == tutees[i].name.compare(this_name)) {  // Tutee already exists
 	  is_new_tutee = false;
 	  tutees[i].visits.push_back(Visit(ConvertCellToTM(row[0]), row[7]));  // Add Visit
@@ -216,6 +265,7 @@ int main(int argc,  char **argv)
     
     cout << "The number of unique visitors is " << tutees.size() << "." << std::endl;
     cout << "The total number of visits was " << num_visits << "." << std::endl;
+    cout << "Note: this is disregarding checkins for courses not supported." << std::endl;
   }
 
   else if (0 == analysis_mode.compare("count_by_course")) {
@@ -251,15 +301,19 @@ int main(int argc,  char **argv)
   }
 
   
-  else if (0 == analysis_mode.compare("print_tutees_and_visits")) {
-    for (auto t : tutees) {
+  else if (0 == analysis_mode.compare("print_tutees")) {
+    for (auto t : tutees)
+      //cout << t.id_number << std::endl;
       cout << t.id_number << "\t" << t.name << "\t" << std::endl;
-      
+  }
+
+  
+  else if (0 == analysis_mode.compare("print_tutees_and_visits")) {
+    for (auto t : tutees) {      
       for (auto v : t.visits)
-	cout << v.course_number << "\t" << v.visit_tm.tm_hour << "h" << v.visit_tm.tm_min << "m\t" << v.visit_tm.tm_mon << "/"
-	     << v.visit_tm.tm_mday << "/" << v.visit_tm.tm_year << "\t" << v.visit_tm.tm_wday << std::endl;
-      
-      cout << std::endl;
+	cout << t.id_number << "\t" << t.name << "\t" << v.course_number << "\t"
+	     << v.visit_tm.tm_hour << "h" << v.visit_tm.tm_min << "m\t" << v.visit_tm.tm_mon << "/"
+	     << v.visit_tm.tm_mday << "/" << v.visit_tm.tm_year + 1900 << "\t" << v.visit_tm.tm_wday << "\t" << v.visit_tm.tm_yday << std::endl;
     }
   }
 
@@ -268,5 +322,47 @@ int main(int argc,  char **argv)
     for (auto t : tutees)
       cout << t.email << std::endl;
 
+  }
+
+
+
+  else if (0 == analysis_mode.compare("pre_and_post_withdrawal")) {
+    std::vector<Tutee> pre_withdrawal_tutees;
+    std::vector<Tutee> post_withdrawal_tutees;
+    const int withdrawal_tm_yday = 57;
+
+    for (auto t : tutees) {
+      if (withdrawal_tm_yday > t.visits[0].visit_tm.tm_yday)  // Tutee first visited Center prior to withdrawal deadline
+	pre_withdrawal_tutees.push_back(t);
+
+      else
+	post_withdrawal_tutees.push_back(t);
+    }
+
+    for (auto t : pre_withdrawal_tutees) {
+      cout << t.id_number << "\t" << t.name << "\t" << "PRE\t" << t.visits[0].visit_tm.tm_yday << std::endl;
+      /*
+      for (auto v : t.visits)
+	cout << t.id_number << "\t" << t.name << "\t" << v.course_number << "\t"
+	     << v.visit_tm.tm_hour << "h" << v.visit_tm.tm_min << "m\t" << v.visit_tm.tm_mon << "/"
+	     << v.visit_tm.tm_mday << "/" << v.visit_tm.tm_year + 1900 << "\t" << v.visit_tm.tm_wday << "\t" << v.visit_tm.tm_yday << std::endl;
+      */
+    }
+
+    cout << "\n\n" << std::endl;
+    for (auto t : post_withdrawal_tutees) {
+      cout << t.id_number << "\t" << t.name << "\t" << "POST\t" << t.visits[0].visit_tm.tm_yday << std::endl;
+      /*
+      for (auto v : t.visits)
+	cout << t.id_number << "\t" << t.name << "\t" << v.course_number << "\t"
+	     << v.visit_tm.tm_hour << "h" << v.visit_tm.tm_min << "m\t" << v.visit_tm.tm_mon << "/"
+	     << v.visit_tm.tm_mday << "/" << v.visit_tm.tm_year + 1900 << "\t" << v.visit_tm.tm_wday << "\t" << v.visit_tm.tm_yday << std::endl;
+      */
+    }
+  }
+
+
+  else if (0 == analysis_mode.compare("usage_by_week")) {
+    int visit_count[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   }
 }
